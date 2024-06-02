899bbfa31f9c093da0f388b292d8c9ddb08d92ac
Mahmoud Odeh <mah881999@gmail.com>
2024-06-02 13:23:52 +0300

    catalog page done

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['e-giftly.com'],
+  },
   basePath: process.env.BASEPATH,
   reactStrictMode: true
 }
--- src/app/[lang]/(dashboard)/home/page.tsx
+++ src/app/[lang]/(dashboard)/home/page.tsx
@@ -1,35 +1,17 @@
 import { fetchData } from '@/helpers/fetcher'
 import Catalog from '../../_components/Catalog/Catalog'
 
-const data = [
-  {
-    title: 'PlayStation',
-    image: 'https://e-giftly.com/images/brands/ps4.jpg',
-    link: '/'
-  },
-  {
-    title: 'PUBG Battlegrounds',
-    image: 'https://e-giftly.com/images/brands/pubg.jpg',
-    link: '/'
-  },
-  {
-    title: 'Fortnite',
-    image: 'https://e-giftly.com/images/brands/fortnite.jpg',
-    link: '/about'
-  }
-]
-
 export default async function Page() {
   const dataCatalog = await fetchData({
     url: "/categories"
   })
 
-  console.log('dataCatalog', dataCatalog)
-  
+  const data = dataCatalog?.data
+  console.log('data', data)
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
-      {data.map((el, index) => (
-        <Catalog key={index} link={el.link} title={el.title} image={el.image} />
+      {data.map((el : any) => (
+        <Catalog key={el.id} id={el.id} title={el.name} image={el.image} has_subcategories={el.has_subcategories}/>
       ))}
     </div>
   )
